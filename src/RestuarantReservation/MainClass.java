3573f91f31ff0b6fe069b32ac84573b8600d5b95
thavath <thavathrath@gmail.com>
2018-05-18 08:46:57 +0700

    reservation part

--- src/RestuarantReservation/MainClass.java
+++ src/RestuarantReservation/MainClass.java
@@ -1,24 +1,77 @@
 package RestuarantReservation;
 
-public class MainClass {
+import java.util.Scanner;
+
+	public class MainClass{
+		public static void menu() {
+			System.out.println("================================================================");
+			System.out.println("================================================================");
+			System.out.println("	========  (1)==New--Table		========");
+			System.out.println("	========  (2)==List--of--Tables		========");
+			System.out.println("	========  (3)==New--Food		========");
+			System.out.println("	========  (4)==List--of--Foods		========");
+			System.out.println("	========  (5)==Take--Reservation	========");
+			System.out.println("	========  (6)==List--of--Reservations	========");
+			System.out.println("	========  (7)==Take--Order		========");
+			System.out.println("	========  (8)==List--of--Orders		========");
+			System.out.println("	========  (9)==Exit--the--Program	========");
+			System.out.println("================================================================");
+			
+	}
 	public static void main(String[] args) {
-		System.out.println("================================================================");
-		System.out.println("================================================================");
-		System.out.println("	========  (1)==New--Table		========");
-		System.out.println("	========  (2)==List--of--Tables		========");
-		System.out.println("	========  (3)==New--Food		========");
-		System.out.println("	========  (4)==List--of--Foods		========");
-		System.out.println("	========  (5)==Take--Reservation	========");
-		System.out.println("	========  (6)==List--of--Reservations	========");
-		System.out.println("	========  (7)==Take--Order		========");
-		System.out.println("	========  (8)==List--of--Orders		========");
-		System.out.println("	========  (9)==Exit--the--Program	========");
-		System.out.println("================================================================");
-		System.out.println("================  Please--Input--Your--Choice : ");
-		Booking b = new Booking();
-		b.reservedBooking();
-//		Table t = new Table();
-//		t.createTable();
-//		t.showTable();
+		
+		Table ta= new Table();
+		Food f = new Food();
+		
+		Scanner input = new Scanner(System.in);
+		
+		try {
+			do {
+				menu();
+				System.out.println("\t\t---------------------- Please--Input--Your--Choice : ");
+			
+				while (!input.hasNextInt()) {
+		             System.out.print("You must enter a valid number! Try again: ");
+		             input.next();
+		       } 
+				int choice;
+				choice = input.nextInt();
+		
+				switch(choice) {
+			
+				case 1:	
+					ta.createTable();
+					break;
+				case 2:
+					System.out.println(ta.toString());
+					break;
+				case 3:
+					f.createFood();
+					break;
+				case 4:
+					System.out.println(f.toString());
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				case 7:
+					break;
+				case 8:
+					break;
+				case 9:
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Invalid Number");                    
+		                break;
+				}
+				System.out.println("Do you want to continue to menu form?[Y/N]");
+				input.nextLine().trim();
+			 } while (input.nextLine().equalsIgnoreCase("y"));
+		
+		}finally {
+	          input.close();
+	    }	
 	}
 }
--- src/RestuarantReservation/Reservation.java
+++ src/RestuarantReservation/Reservation.java
@@ -22,20 +22,27 @@ public class Reservation implements CRUD<Booking>{
 	}
 
 	public void create(Booking b) {
-		
+		b = new Booking();
+		b.reservedBooking();
 	}
 
 	public ArrayList<Booking> read() {
-		
+		Booking b = new Booking();
+		System.out.println(b.toString());
+		for (int i = 0; i < b.getReservedTable().size(); i++) {
+			System.out.println(" #"+b.getReservedTable().get(i).getReserveTable());
+		}
 		return null;
 	}
 
 	public void update(Booking b) {
-		
+		for (int i = 0; i < b.getReservedTable().size(); i++) {
+			System.out.println(" #"+b.getReservedTable().get(i).getReserveTable());
+		}
 	}
 
 	public void delete(Booking b) {
-		
+		b = null;
 	}
 
 	public String toString() {
